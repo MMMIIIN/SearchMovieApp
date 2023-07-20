9b93e441ea74207c5e0056c75ce2f0febe83c96b
chemi <wbsldj7645@naver.com>
2023-07-20 15:45:32 +0900

    first commit

---
+++ SearchMovieApp/SearchMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchMovieApp
+//
+//  Created by Mingwan Choi on 2023/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SearchMovieApp/SearchMovieAppUITests/SearchMovieAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SearchMovieAppUITestsLaunchTests.swift
+//  SearchMovieAppUITests
+//
+//  Created by Mingwan Choi on 2023/07/20.
+//
+
+import XCTest
+
+final class SearchMovieAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
