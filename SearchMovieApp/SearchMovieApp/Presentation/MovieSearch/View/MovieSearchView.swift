0d15a2a4d8ed4fd7202594eeb013f4630b4edaa9
chemi <wbsldj7645@naver.com>
2023-07-21 16:42:04 +0900

    [ADD] SearchBar 생성

--- SearchMovieApp/SearchMovieApp/Presentation/MovieSearch/View/MovieSearchView.swift
+++ SearchMovieApp/SearchMovieApp/Presentation/MovieSearch/View/MovieSearchView.swift
@@ -5,4 +5,49 @@
 //  Created by Mingwan Choi on 2023/07/20.
 //
 
-import Foundation
+import FlexLayout
+import PinLayout
+
+import UIKit
+
+final class MovieSearchView: UIView {
+    
+    // MARK: - ui component
+    
+    private let flexContainerView = UIView()
+    private let movieSearchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        return searchBar
+    }()
+    
+    // MARK: - init
+    
+    override init (frame: CGRect) {
+        super.init(frame: .zero)
+        self.setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - override
+    
+    override func layoutSubviews() {
+        self.flexContainerView.pin.all(self.pin.safeArea)
+        self.flexContainerView.flex.layout(mode: .fitContainer)
+    }
+    
+    // MARK: - func
+    
+    func delegationSearchBar(_ viewController: MovieSearchViewController) {
+        self.movieSearchBar.delegate = viewController
+    }
+    
+    private func setupLayout() {
+        self.flexContainerView.flex.direction(.column).justifyContent(.start).define { flex in
+            flex.addItem(self.movieSearchBar)
+        }
+        self.addSubview(self.flexContainerView)
+    }
+}
--- SearchMovieApp/SearchMovieApp/Presentation/MovieSearch/View/MovieSearchViewController.swift
+++ SearchMovieApp/SearchMovieApp/Presentation/MovieSearch/View/MovieSearchViewController.swift
@@ -9,4 +9,24 @@ import UIKit
 
 final class MovieSearchViewController: UIViewController {
     
+    // MARK: - property
+    
+    private let movieSearchView = MovieSearchView()
+    
+    // MARK: - life cycle
+    
+    override func loadView() {
+        self.view = movieSearchView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+//        self.movieSearchView.delegationSearchBar(self)
+    }
+    
+}
+
+extension MovieSearchViewController: UISearchBarDelegate {
+    
 }
