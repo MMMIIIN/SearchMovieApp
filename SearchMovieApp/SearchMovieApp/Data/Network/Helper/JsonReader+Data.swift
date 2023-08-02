0d8625a7f93fb4d1ebdfb6895d252e249c0c21fa
chemi <wbsldj7645@naver.com>
2023-08-02 10:41:21 +0900

    [ADD] mock data 추가

---
+++ SearchMovieApp/SearchMovieApp/Data/Network/Helper/JsonReader+Data.swift
@@ -0,0 +1,18 @@
+//
+//  JsonReader+Data.swift
+//  SearchMovieApp
+//
+//  Created by Mingwan Choi on 2023/08/01.
+//
+
+import Foundation
+
+extension Data {
+    static func loadMockMovieList() -> Data {
+        guard let path = Bundle.main.path(forResource: "MockMovieList", ofType: "json") else { return Data() }
+        guard let jsonString = try? String(contentsOfFile: path) else { return Data() }
+        
+        guard let encodeData = jsonString.data(using: String.Encoding.utf8) else { return Data() }
+        return encodeData
+    }
+}
