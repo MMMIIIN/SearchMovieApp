15ef3b77f86511a726b6a2ab4f16d57ba999102b
chemi <wbsldj7645@naver.com>
2023-08-08 16:45:41 +0900

    [ADD] 400에러 stub 함수 추가

--- SearchMovieApp/SearchMovieAppTests/Data/Network/Mock/MockMovieSearchService.swift
+++ SearchMovieApp/SearchMovieAppTests/Data/Network/Mock/MockMovieSearchService.swift
@@ -12,14 +12,14 @@ import Moya
 
 final class MockMovieSearchService: MovieSearchService {
     func fetchNowPlayingMovies() async throws -> Result<[Movie], NetworkError> {
-        let searchMovieEndpointClosure = { (target: MovieSearchEndPoint) -> Endpoint in
+        let nowPlayingEndpointClosure = { (target: MovieSearchEndPoint) -> Endpoint in
             return Endpoint(url: URL(target: target).absoluteString,
                             sampleResponseClosure: { .networkResponse(200, target.sampleData) },
                             method: target.method,
                             task: target.task,
                             httpHeaderFields: target.headers)
         }
-        let provider = MoyaProvider<MovieSearchEndPoint>(endpointClosure: searchMovieEndpointClosure, stubClosure: MoyaProvider<MovieSearchEndPoint>.immediatelyStub)
+        let provider = MoyaProvider<MovieSearchEndPoint>(endpointClosure: nowPlayingEndpointClosure, stubClosure: MoyaProvider<MovieSearchEndPoint>.immediatelyStub)
         
         return try await withCheckedThrowingContinuation { continuation in
             provider.request(.nowPlaying) { result in
@@ -29,6 +29,35 @@ final class MockMovieSearchService: MovieSearchService {
                     continuation.resume(returning: .success(movieList.toMovieList()))
                 case .failure:
                     continuation.resume(returning: .failure(.unknownError))
+                }
+            }
+        }
+    }
+    
+    func fetchNowPlayingMovies400Error() async throws -> Result<[Movie], NetworkError> {
+        let nowPlaying400EndpointClosure = { (target: MovieSearchEndPoint) -> Endpoint in
+            return Endpoint(url: URL(target: target).absoluteString,
+                            sampleResponseClosure: { .networkResponse(400, Data()) },
+                            method: target.method,
+                            task: target.task,
+                            httpHeaderFields: target.headers)
+        }
+        let provider = MoyaProvider<MovieSearchEndPoint>(endpointClosure: nowPlaying400EndpointClosure, stubClosure: MoyaProvider<MovieSearchEndPoint>.immediatelyStub)
+        
+        return try await withCheckedThrowingContinuation { continuation in
+            provider.request(.nowPlaying) { result in
+                switch result {
+                case .success(let response):
+                    switch response.statusCode {
+                    case 200..<300:
+                        continuation.resume(returning: .success([]))
+                    case 400..<500:
+                        continuation.resume(throwing: NetworkError.unknownError)
+                    default:
+                        continuation.resume(throwing: NetworkError.serverError)
+                    }
+                case .failure:
+                    continuation.resume(throwing: NetworkError.serverError)
                 }
             }
         }
@@ -56,5 +85,33 @@ final class MockMovieSearchService: MovieSearchService {
             }
         }
     }
+    
+    func fetchSearchMovie400Error(query title: String) async throws -> Result<[Movie], NetworkError> {
+        let searchMovie400EndpointClosure = { (target: MovieSearchEndPoint) -> Endpoint in
+            return Endpoint(url: URL(target: target).absoluteString,
+                            sampleResponseClosure: { .networkResponse(400, target.sampleData) },
+                            method: target.method,
+                            task: target.task,
+                            httpHeaderFields: target.headers)
+        }
+        let provider = MoyaProvider<MovieSearchEndPoint>(endpointClosure: searchMovie400EndpointClosure, stubClosure: MoyaProvider<MovieSearchEndPoint>.immediatelyStub)
+        
+        return try await withCheckedThrowingContinuation { continuation in
+            provider.request(.searchMovie(query: title)) { result in
+                switch result {
+                case .success(let response):
+                    switch response.statusCode {
+                    case 200..<300:
+                        continuation.resume(returning: .success([]))
+                    case 400..<500:
+                        continuation.resume(throwing: NetworkError.unknownError)
+                    default:
+                        continuation.resume(throwing: NetworkError.serverError)
+                    }
+                case .failure:
+                    continuation.resume(throwing: NetworkError.unknownError)
+                }
+            }
+        }
+    }
 }
-
