21f1119f3e71bb6bd23ff56d8478a6b8384bac1b
chemi <wbsldj7645@naver.com>
2023-08-08 17:18:23 +0900

    [ADD] Repository 테스트 코드 작성

--- SearchMovieApp/SearchMovieAppTests/Data/Repository/MovieSearchRepositoryTest.swift
+++ SearchMovieApp/SearchMovieAppTests/Data/Repository/MovieSearchRepositoryTest.swift
@@ -9,13 +9,32 @@ import XCTest
 @testable import SearchMovieApp
 
 final class MovieSearchRepositoryTest: XCTestCase {
-    func testRequest() async throws {
-        let repository = DefaultMovieSearchRepository()
-        
-        let movies = try await repository.loadNowPlayingMovies()
-        
-        print(movies)
-        
-        XCTAssertNotNil(movies)
+    
+    var repository: MovieSearchRepository!
+    
+    override func setUp() {
+        self.repository = DefaultMovieSearchRepository(movieSearchService: MockMovieSearchService())
+    }
+    
+    override func tearDown() {
+        self.repository = nil
+    }
+    
+    func test_loadNowPlayingMovies함수가_에러없이_정상적으로_리턴하는가() async throws {
+        do {
+            let movies = try await self.repository.loadNowPlayingMovies()
+            XCTAssertFalse(movies.isEmpty)
+        } catch {
+            XCTFail("Error")
+        }
+    }
+    
+    func test_searchMovie함수가_에러없이_정상적으로_리턴하는가() async throws {
+        do {
+            let movies = try await self.repository.searchMovie(query: "범죄도시")
+            XCTAssertFalse(movies.isEmpty)
+        } catch {
+            XCTFail("Error")
+        }
     }
 }
