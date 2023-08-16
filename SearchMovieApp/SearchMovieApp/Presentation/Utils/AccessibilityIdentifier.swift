5e9a93ee6f3e4fd21bf468d63317c3dc02b4893b
chemi <wbsldj7645@naver.com>
2023-08-16 21:42:49 +0900

    [ADD] UITest를 위한 AccessibilityIdentifier 추가

---
+++ SearchMovieApp/SearchMovieApp/Presentation/Utils/AccessibilityIdentifier.swift
@@ -0,0 +1,12 @@
+//
+//  AccessibilityIdentifier.swift
+//  SearchMovieApp
+//
+//  Created by Mingwan Choi on 2023/08/15.
+//
+
+import Foundation
+
+struct AccessibilityIdentifier {
+    static let movieSearchBarTextField = "MovieSearchBarTextField"
+}
